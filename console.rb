0f7bb76f0636292210d39b59bbf160fa9c6b4fff
James Monks <J.monks23@gmail.com>
2020-07-01 14:37:08 +0100

    added album save/list_all/delete_all and connected album and artist

--- console.rb
+++ console.rb
@@ -1,18 +1,22 @@
 require("pry-byebug")
 require_relative("models/artist")
+require_relative("models/album")
 
+Album.delete_all()
 Artist.delete_all()
+
 
 artist1 = Artist.new({
     "name" => "Johnny Cash"
 })
 artist1.save()
 
-
-
-
-
-
+album1 = Album.new({ 
+    "title" => "Orange Blossom Special",
+    "genre" => "Rock",
+    "artist_id" => artist1.id
+    })
+album1.save()
 
 
 binding.pry
--- models/album.rb
+++ models/album.rb
@@ -12,4 +12,25 @@ class Album
         @artist_id = options["artist_id"].to_i()
     end
 
+    def save()
+        sql = "INSERT INTO albums
+        (title, genre, artist_id)
+        VALUES
+        ($1, $2, $3) RETURNING id" 
+        values = [@title, @genre, @artist_id]
+        result = SqlRunner.run(sql, values)
+        @id = result[0]["id"].to_i()
+     end
+     
+     def self.all()
+        sql = "SELECT * FROM albums"
+        result = SqlRunner.run(sql)
+        return result.map { |album| Album.new(album) }
+    end
+
+    def self.delete_all()
+        sql = "DELETE FROM albums"
+        result = SqlRunner.run(sql)
+    end
+
 end
